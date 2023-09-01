ba496f3ad225c8a0b992f30261856fab923a510f
kabir hossain <kabirhossain@kabirs-MacBook-Air.local>
2023-09-01 16:53:19 +0600

    feat: add feedback from and display feedback from

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
-	
+	import FeedbackForm from './components/UI/FeedbackForm.svelte';
+	import FeedbackStats from './components/UI/FeedbackStats.svelte';
+	import FeedbackList from './components/FeedbackList.svelte';
 </script>
 
-<main>
-	<h1>Hello!</h1>
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
 </main>
-
